2ed64847a77b9325889fec0aa4ec7b0b2a829fd0
Mikael Vinicius <viniciusmikael943@gmail.com>
2023-02-11 14:53:09 -0300

    Criação do projeto

---
+++ conversorDolar/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Digite a quantidade em Dolar a ser convetitida "
+				+ " para Real");
+		float valor = sc.nextInt();
+		
+		float valorBruto =  valor * 5.22f;
+		
+		System.out.printf("U$" + valor + " equivale a R$%.2f ", valorBruto);
+
+	}
+
+}
